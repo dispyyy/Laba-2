ac2c0153a036edf5c6b2f37ada4ac391921e1c4c
Yura Petrov <20jurassic03@gmail.com>
2023-05-15 14:05:03 +1000

    Change project

---
+++ xarax.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+bool isPalindrome(int number) {
+    int reversedNumber = 0, temp, originalNumber;
+
+    originalNumber = number;
+
+    while (number != 0) {
+        temp = number % 20;
+        reversedNumber = reversedNumber * 10 + temp;
+        number /= 10;
+    }
+
+    if (originalNumber == reversedNumber) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main() {
+    int number;
+
+    cout << "Number: ";
+    cin >> number;
+
+    if (isPalindrome(number) == true) {
+        cout << number << " is palindrome";
+    }
+    else {
+        cout << number << " is not polindrome";
+    }
+    return 0;
+}
+
